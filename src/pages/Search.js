353fc810e9fa1c868acd3887bf20b095ccd23ca8
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-04 15:06:12 -0300

    Requisito 5 finalizado.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,67 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      choosedArtist: '',
+      isDisabled: true,
+    };
+  }
+
+  validateArtist = () => {
+    const { choosedArtist } = this.state;
+    const validator = 2;
+    const returnBool = choosedArtist.length < validator;
+
+    this.setState({
+      isDisabled: returnBool,
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    return this.setState({
+      [name]: value,
+    }, () => this.validateArtist());
+  };
+
   render() {
+    const {
+      choosedArtist,
+      isDisabled,
+    } = this.state;
+
     return (
       <div
         data-testid="page-search"
       >
-        <Header />
+        <div>
+          <Header />
+        </div>
+        <div>
+          <form>
+            <label htmlFor="search">
+              Search:
+              <input
+                id="artist"
+                name="choosedArtist"
+                data-testid="search-artist-input"
+                value={ choosedArtist }
+                placeholder="O que você quer ouvir?"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
