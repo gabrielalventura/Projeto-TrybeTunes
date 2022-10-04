5f2849a5315210526b47c43fb616ebb2729220e6
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-03 21:28:45 -0300

    Requisito 3 finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      username: await getUser(),
+    });
+  }
+
   render() {
+    const {
+      username,
+    } = this.state;
+
     return (
       <header
         data-testid="header-component"
       >
-        <p>Recuperar nome</p>
+        {username === '' ? (
+          <Loading />
+        ) : (
+          <h2 data-testid="header-user-name">
+            {username.name}
+          </h2>
+        )}
       </header>
     );
   }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Search extends React.Component {
   render() {
     return (
       <div
         data-testid="page-search"
-      />
+      >
+        <Header />
+      </div>
     );
   }
 }
