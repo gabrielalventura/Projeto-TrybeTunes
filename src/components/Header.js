9a75558353a871e5b0271cdc664d8ed7ec7eb34b
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-03 21:42:50 -0300

    Requisito 4 finalizado. MDDC

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,27 @@ class Header extends React.Component {
             {username.name}
           </h2>
         )}
+
+        <Link
+          to="/search"
+          data-testid="link-to-search"
+        >
+          Search
+        </Link>
+
+        <Link
+          to="/favorites"
+          data-testid="link-to-favorites"
+        >
+          Liked
+        </Link>
+
+        <Link
+          to="/profile"
+          data-testid="link-to-profile"
+        >
+          Profile
+        </Link>
       </header>
     );
   }
