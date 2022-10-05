ec58e8939923755838dc2de119009bfc9a9dd483
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-05 17:39:32 -0300

    Requisito 8 finalizado

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      load: false,
+      favouritesMusics: false,
+    };
+  }
+
+  handleChangeFavourite = async ({ target }) => {
+    const { name, checked } = target;
+    const { music } = this.props;
+
+    this.setState({
+      load: true,
+    });
+
+    await addSong(music);
+
+    this.setState({
+      load: false,
+      [name]: checked,
+    });
+  }; // função handleChange desenvolviada com auxilio da Maria Luíza Suhadolnik;
+
   render() {
     const { music } = this.props;
+    const {
+      load,
+      favouritesMusics,
+    } = this.state;
 
     return (
       <section className="album-music">
@@ -17,14 +47,18 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+
+        {load && <Loading />}
         <label
-          htmlFor={ music.trackName }
-          data-testid={ `checkbox-music-${music.trackId}` }
+          htmlFor="favouritesMusics"
         >
           <input
-            name="liked"
-            id={ music.trackName }
+            data-testid={ `checkbox-music-${music.trackId}` } // colocando o data-testid dentro da tag da label não passava nos testes
+            name="favouritesMusics"
+            id={ music.trackId }
             type="checkbox"
+            checked={ favouritesMusics }
+            onChange={ this.handleChangeFavourite }
           />
           Favorita
         </label>
@@ -34,7 +68,7 @@ class MusicCard extends React.Component {
 }
 
 MusicCard.propTypes = {
-  music: PropTypes.object,
+  music: PropTypes.arrayOf(),
 }.isRequired;
 
 export default MusicCard;
