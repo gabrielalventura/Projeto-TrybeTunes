fab2e758fb7fbaf721e8c57e52790bfc5296b575
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-06 10:52:17 -0300

    Requisito 9 passando nos testes

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,68 +1,98 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor() {
     super();
     this.state = {
       load: false,
-      favouritesMusics: false,
+      // favoritesMusics: false,
+      selectedFavorites: [],
     };
   }
 
-  handleChangeFavourite = async ({ target }) => {
+  componentDidMount() {
+    this.takeFavoriteMusics();
+  }
+
+  takeFavoriteMusics = async () => {
+    this.setState({
+      load: true,
+    });
+    const list = await getFavoriteSongs();
+
+    this.setState({
+      selectedFavorites: list,
+      load: false,
+    }, () => this.seekFavorite());
+  };
+
+  seekFavorite = () => {
+    const { music } = this.props;
+    const { selectedFavorites } = this.state;
+
+    return selectedFavorites.some((liked) => liked.trackId === music.trackId);
+  };
+
+  handleChangeFavorite = async ({ target }) => {
     const { name, checked } = target;
     const { music } = this.props;
 
     this.setState({
       load: true,
+      [name]: checked,
     });
 
-    await addSong(music);
+    if (this.seekFavorite()) {
+      await removeSong(music);
+    } else {
+      await addSong(music);
+    }
 
     this.setState({
       load: false,
-      [name]: checked,
     });
+    this.takeFavoriteMusics();
   }; // função handleChange desenvolviada com auxilio da Maria Luíza Suhadolnik;
 
   render() {
     const { music } = this.props;
     const {
       load,
-      favouritesMusics,
+      // favoritesMusics,
     } = this.state;
 
     return (
-      <section className="album-music">
-        <span>
-          { music.trackName }
-        </span>
-        <audio data-testid="audio-component" src={ music.previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
+      !load
+        ? (
+          <section className="album-music">
+            <span>
+              {music.trackName}
+            </span>
+            <audio data-testid="audio-component" src={ music.previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
 
-        {load && <Loading />}
-        <label
-          htmlFor="favouritesMusics"
-        >
-          <input
-            data-testid={ `checkbox-music-${music.trackId}` } // colocando o data-testid dentro da tag da label não passava nos testes
-            name="favouritesMusics"
-            id={ music.trackId }
-            type="checkbox"
-            checked={ favouritesMusics }
-            onChange={ this.handleChangeFavourite }
-          />
-          Favorita
-        </label>
-      </section>
+            <label
+              htmlFor="likedMusics"
+            >
+              <input
+                data-testid={ `checkbox-music-${music.trackId}` } // colocando o data-testid dentro da tag da label não passava nos testes
+                name="likedMusics"
+                id={ music.trackId }
+                type="checkbox"
+                checked={ this.seekFavorite() }
+                onChange={ this.handleChangeFavorite }
+              />
+              Favorita
+            </label>
+          </section>) : <Loading />
     );
   }
 }
